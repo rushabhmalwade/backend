d153319ac5183d3ba056b86e9f37b9b4332954e2
RishabhMalwade <rushabhmalwade@gmail.com>
2025-05-25 18:35:56 +0530

    basic implementation of using controller and router at the same time

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import express from "express";
 import testRouter from "./routes/testRoutes.js";
+import userRouter from "./routes/user/userRoutes.js";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.use("/", (req, res, next) => {
-  res.send("I am app.use");
-});
+// app.use("/", (req, res, next) => {
+//   res.send("I am app.use");
+// });
 
 app.all("/secret", (req, res) => {
   res.send("Hi i am secret");
@@ -21,6 +22,8 @@ app.get("/test", (req, res) => {
 app.use(testRouter); //this will make /test route available
 
 app.use("/dev", testRouter); ////this will make /dev/test route available
+
+app.use("/users", userRouter);
 
 app.get("/hello", (req, res) => {
   res.send("<h1>Hello from the user</h1>");
---
+++ routes/user/userController.js
@@ -0,0 +1,5 @@
+const getAllUsers = (req, res) => {
+  res.send("<h1>Hello from the User Controller!</h1>");
+};
+
+export default getAllUsers;
